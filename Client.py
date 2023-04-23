96557bc059b19e20a86b666530f47174c9487ae4
vinibleik <vinicius_baggio@hotmail.com>
2023-04-23 15:40:27 -0300

    Added assymetric and symmetric keys to change messages safely between two Clients

--- Client.py
+++ Client.py
@@ -1,14 +1,24 @@
 #! /usr/bin/env python
 
+import base64
 import select
 import socket
 import sys
 import traceback
+from random import randint
 from threading import Thread
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric.x25519 import (
+    X25519PrivateKey,
+    X25519PublicKey,
+)
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 DEFAULT_HOST = "127.0.0.1"
 DEFAULT_PORT = 5535
-DEFAULT_BUFFSIZE = 1024
+DEFAULT_BUFSIZE = 1024
 
 
 class Client:
@@ -17,17 +27,41 @@ class Client:
         user_name: str,
         host: str = DEFAULT_HOST,
         port: int = DEFAULT_PORT,
-        buffsize: int = DEFAULT_BUFFSIZE,
+        bufsize: int = DEFAULT_BUFSIZE,
     ) -> None:
         self.user_name = user_name
         self.host = host
         self.port = port
-        self.buffsize = buffsize
+        self.bufsize = bufsize
+        self.__init_keys()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.configure_socket()
+        self.__configure_socket()
         self.run()
 
-    def configure_socket(self) -> None:
+    def __init_keys(self) -> None:
+        self.__private_key: X25519PrivateKey = X25519PrivateKey.generate()
+        self.public_key: X25519PublicKey = self.__private_key.public_key()
+        self.__shared_key: bytes = b""
+        self.__derived_key: bytes = b""
+        self.__symmetric_key: Fernet | None = None
+
+    def __generate_derived_key(self, peer_public_key: bytes) -> None:
+        public_key = X25519PublicKey.from_public_bytes(peer_public_key)
+        self.__shared_key = self.__private_key.exchange(public_key)
+        self.__derived_key = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=None,
+            info=b"handshake data",
+        ).derive(self.__shared_key)
+
+    def __generate_symmetric_key(self, peer_public_key: bytes = b"") -> None:
+        if peer_public_key != b"":
+            self.__generate_derived_key(peer_public_key)
+        urlsafe_b64_encode = base64.urlsafe_b64encode(self.__derived_key)
+        self.__symmetric_key = Fernet(urlsafe_b64_encode)
+
+    def __configure_socket(self) -> None:
         self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
     def run(self) -> None:
@@ -39,6 +73,21 @@ class Client:
             print(
                 f"Connected to {client.getpeername()} as {self.user_name}:{client.getsockname()}"
             )
+            while self.__symmetric_key is None:
+                msg = "public_key,".encode()
+                msg += self.public_key.public_bytes_raw()
+                self.socket.send(msg)
+                data = self.socket.recv(self.bufsize)
+                if data != b"":
+                    type, peer_public_key = data.split(b",")
+                    self.__generate_symmetric_key(
+                        peer_public_key=peer_public_key
+                    )
+                    if type == b"public_key":
+                        msg = "response,".encode()
+                        msg += self.public_key.public_bytes_raw()
+                        self.socket.send(msg)
+
             thread_send = Thread(target=self.send)
             thread_listen = Thread(target=self.listen, daemon=True)
             thread_send.start()
@@ -49,16 +98,18 @@ class Client:
         while (msg := input(">> ")) != "exit":
             if msg == "":
                 continue
-            data = f"{self.user_name}: {msg}"
-            self.socket.send(data.encode())
+            data = f"{self.user_name}: {msg}".encode()
+            encrypted_data = self.__symmetric_key.encrypt(data)
+            self.socket.send(encrypted_data)
         self.socket.send(msg.encode())
 
     def listen(self) -> None:
         while True:
             [read], _, _ = select.select([self.socket], [], [])
             try:
-                msg = read.recv(self.buffsize)
-                print(f"Message from {msg.decode()}\n>> ", end="")
+                msg = read.recv(self.bufsize)
+                decrypted_msg = self.__symmetric_key.decrypt(msg)
+                print(f"Message from {decrypted_msg.decode()}\n>> ", end="")
             except Exception:
                 traceback.print_exc(file=sys.stdout)
                 break
@@ -68,4 +119,5 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         Client(sys.argv[1])
     else:
-        Client("vinibaggio")
+        user = f"user{randint(0,1_000_000)}"
+        Client(user)
